b94fe88342deb89e82f45d5e1a7c8ab0cf6a0d8c
phizio <ivantsov_rs@mail.ru>
2015-10-06 09:09:05 +0300

    Change syntaxis of alerts'widget

--- templates/bootstrap3/alert.php
+++ templates/bootstrap3/alert.php
@@ -1,4 +1,4 @@
-<div class="alert alert-<?= $v['class'] ?>">
+<div class="alert alert-<?= $class ?>">
     <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
-    <?= $v['msg'] ?>
+    <?= $msg ?>
 </div>
